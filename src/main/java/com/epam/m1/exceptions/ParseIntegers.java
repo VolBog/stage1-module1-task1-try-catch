42da405bd98e9d1fa4b1b518bb0bae67a01204c1
Volodymyr Tsiumra <volodymyr.tsiumra@gds.ey.com>
2023-05-18 15:16:19 +0200

    Finish tasks

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,8 +21,13 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            int number = 0;
+            try{
+                number = Integer.parseInt(next);
+                sum+=number;
+            } catch (NumberFormatException ignored){
+                justWords += " " + next;
+            }
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
